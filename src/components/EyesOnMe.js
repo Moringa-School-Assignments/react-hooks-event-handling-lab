c07021a39dbdf97e138b255c0e0e94ea12348c16
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-09-26 01:16:27 +0300

    complete assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+
+function handleFocus(e){
+console.log("Good!");
+}
+
+function handleBlur(e){
+console.log(
+
+    
+);
+}
+
+function EyesOnMe() {
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,20 @@
 // Code Keypad Component Here
+import React from 'react';
+// import handleChange from "./EyesOnMe";
 
-function Keypad (){
+function handleChange(e){
+    e.preventDefault();
+    console.log("Entering password...");
+}
+
+function Keypad() {
     return (
-        <div></div>
+        <div>
+            <label htmlFor="name">Name (4 to 8 characters):</label>
+
+            <input type="password" id="name" onChange={handleChange} name="name" required
+                minLength="4" maxLength="80" size="40"></input>
+        </div>
     )
 }
 
